9161af6b9ca80b7912e7ebe239f020349b57595f
Sebastiaan Mannem <sebas@mannem.nl>
2014-03-18 19:06:14 +0100

    add --noconfirm to upgrade_arch.sh

--- upgrade_arch.sh
+++ upgrade_arch.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 SS_NAME=$(date +%Y-%m-%d_%H-%M-%S)
-cd /var/lib/btrfs
+BTRFS_LIB=/var/lib/btrfs
+cd $BTRFS_LIB
 umount __active
 btrfs su sn __active __snapshots/$SS_NAME
 mount __active
-pacman -Syu
+pacman -Syu --noconfirm
+ls -tr ./__snapshots/ | sed -n '5,$p' | while read s; do btrfs su de ./__snapshots/$s; done
