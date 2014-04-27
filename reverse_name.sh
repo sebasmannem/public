be58f5e0eba84c8117f45a1a2424e6897c111a89
git daemon user <git@study.mannem.nl>
2014-04-27 16:45:47 +0200

    aanpassing op reverse_name.sh

--- reverse_name.sh
+++ reverse_name.sh
@@ -6,11 +6,19 @@ do
   if [ -f "$i" ]; then
     f=$(basename "$i")
     d=$(dirname "$i")
-    ext=$(echo $f | sed 's/^.*\.//')
-    new=$d/$(echo "$f" | sed 's/\.[^.]*$//' | rev).$ext
-    echo "Moving '$d/$f' to '$new'"
-    mv "$d/$f" "$new"
+    ext=.$(echo $f | sed 's/^.*\.//')
+    [ ${#f} -eq ${#ext} ] && ext=""
+    f=$(basename "$f" "$ext")
+  elif [ -d "$i" ]; then
+    f=$(basename "$i")
+    d=$(dirname "$i")
+    ext=""
   else
     echo "File '$i' does not exist."
+    continue
   fi
+
+  new=$d/$(echo "$f" | rev)
+  echo "Moving '$d/$f$ext' to '$new$ext'"
+  mv "$d/$f$ext" "$new$ext"
 done
