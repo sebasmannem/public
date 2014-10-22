4c4c33d7f78e15f04264c8b47d62b857065385c3
Sebastiaan Mannem <sebas@mannem.nl>
2014-10-22 10:00:24 +0200

    installer script for arch

--- bash/install-arch.sh
+++ bash/install-arch.sh
@@ -1,27 +1,62 @@
-#!/bin/sh
+#!/bin/bash
 
-#Refresh list with:
-#{ pacman -Qqet | awk '{print $1}' ; cat /u02/nas/scripts/conf/package_list.txt ; } | sort -u > package_list.txt
+function quitOnError() {
+  [ $# -gt 0 ] && echo "$@"
+  exit 1
+}
 
-PCKs=$(cat /u02/nas/scripts/conf/package_list.txt)
-echo Trying to install
-echo $PCKs
-echo
-pacman -S --noconfirm ${PCKs} 2>&1 | awk 'BEGIN{FS=":"}$1~/error/{print $3}' > /tmp/pacman_failed_packages.txt
-PCKs_FAILED=$(cat /tmp/pacman_failed_packages.txt | xargs | sed 's/ /|/g')
-if [ "$PCKs_FAILED" ]; then
-  echo Failed on
-  echo $PCKs_FAILED
-  echo
-  PCKs=$(grep -vE "($PCKs_FAILED)" /u02/nas/scripts/conf/package_list.txt)
-  echo Trying to install
-  echo $PCKs
-  echo
-  pacman -S --noconfirm ${PCKs} 
-fi
+HN=new.mannem.nl
+IP=192.168.122.200
+NETMASK=24
+BROADCASt=192.168.122.255
+GATEWAY=192.168.122.1
 
-exit
-cat /u02/nas/scripts/conf/package_list.txt | while read pck
-do
-  pacman -S --noconfirm "$pck" || echo "$pck" >> /tmp/pacman_failed_packages.txt
-done
+[ "$1" ] && DRIVE="$1" || quitOnError "Please specify an empty disk device from /dev/."
+[[ "$DRIVE" =~ / ]] || DRIVE="/dev/$DRIVE"
+[[ "$DRIVE" =~ ^/dev/[svh] ]] || quitOnError "$DRIVE is not an disk device (/dev/[svh]*). Please specify an empty disk device from /dev/."
+[[ "$DRIVE" =~ [0-9]$ ]] && quitOnError "$DRIVE is not an disk device but a partition. Please specify an empty disk device from /dev/."
+[ -b "$DRIVE" ] || quitOnError "$DRIVE does not exist."
+ls "$DRIVE"[0-9] >/dev/null 2>&1 && quitOnError "$DRIVE has a partition. Please specify an empty disk device from /dev/."
+
+echo "installing to $DRIVE"
+parted -s "$DRIVE" mklabel msdos || quitOnError "Could not create partition table"
+parted -s -- "$DRIVE" mkpart primary ext2 1 -0 || quitOnError "Could not create partition"
+parted -s "$DRIVE" set 1 LVM on || quitOnError "Could set LVM on for partition"
+
+pvcreate "${DRIVE}1" || quitOnError "Could not create Physical Disk"
+vgcreate Volume00 "${DRIVE}1" || quitOnError "Could not create LVM Volume Group"
+
+MEM=$(free -b | awk '/Mem:/{print $2}')
+lvcreate -n lv_swap -L $(($MEM*2))b Volume00 || quitOnError "Could not create SWAP logical Volume"
+mkswap /dev/Volume00/lv_swap || quitOnError "Could not initialize swap disk"
+swapon /dev/Volume00/lv_swap || quitOnError "Could not set swap on"
+
+lvcreate -n lv_root -l 100%FREE Volume00 || quitOnError "Could not create ROOT Logical Volume"
+mkfs.btrfs /dev/Volume00/lv_root || quitOnError "Could nog make btrfs filesystem"
+mount /dev/Volume00/lv_root /mnt || quitOnError "COuld not mount btrfs fs"
+mkdir /mnt/__snapshots || quitOnError "Could not make subdir for snapshots"
+btrfs su cr /mnt/__active || quitOnError "Could not create __active subvolume"
+BTRFS_ID=$(btrfs su li /mnt | awk '/__active/{print $2}')
+btrfs su se ${BTRFS_ID} /mnt || quitOnError "Could not set $BTRFS_ID as default subvol"
+umount /mnt || quitOnError "Could not unmount /mnt"
+mount /dev/Volume00/lv_root /mnt || quitOnError "Could not remount btrfs fs"
+mkdir -p /mnt/var/lib/btrfs/empty || quitOnError "Could not create folder /mnt/var/lib/btrfs/empty/"
+pacstrap /mnt base || quitOnError "Could not pacstrap"
+
+genfstab -p /mnt > /mnt/etc/fstab
+echo '/var/lib/btrfs/empty                            /var/lib/btrfs/__active none    bind' >> /mnt/etc/fstab
+
+echo "$HN" > /mnt/etc/hostname
+
+echo "address=$IP
+netmask=$NETMASK
+broadcast=$BROADCAST
+gateway=$GATEWAY" > /mnt/etc/conf.d/network\@eth0
+cp "/u01/git/scripts/systemd/network@.service" "/mnt/etc/systemd/system/network@.service"
+
+mkdir -p /mnt/u02/nas
+mount -o bind /u02/nas /mnt/u02/nas
+
+ln -sf /usr/share/zoneinfo/Europe/Amsterdam /mnt/etc/localtime
+
+arch-chroot /mnt /u02/nas/git/scripts/bash/install-arch-chroot.sh | tee -a /mnt/tmp/install-arch-chroot.log
