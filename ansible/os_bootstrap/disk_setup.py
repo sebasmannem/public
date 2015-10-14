0fe731441d7e44d04f21d48cb42ea295e0b91916
Sebastiaan Mannem <sebas@mannem.nl>
2015-10-15 01:10:06 +0200

    disk_setup.py working version

--- ansible/os_bootstrap/disk_setup.py
+++ ansible/os_bootstrap/disk_setup.py
@@ -4,12 +4,20 @@ import re
 def blkid2dict(blkid):
   ret={}
   splitter_re=re.compile('[ \t]+')
-  for pair in splitter_re(blkid):
-    key, val = par.split('=', 1)
-    if val[0] == '"' and val[-1] == '"':
-      val = val[1:-1]
-    ret[key] = val
-  return retblkid2dict
+  for pair in splitter_re.split(blkid.decode('utf-8')):
+    try:
+      if pair[-1] == '\n':
+        pair=pair[:-1]
+    except:
+      pass
+    try:
+      key, val = pair.split('=', 1)
+      if val[0] == '"' and val[-1] == '"':
+        val = val[1:-1]
+      ret[key] = val
+    except:
+      pass
+  return ret
 
 if __name__ == "__main__":
   from glob import glob
@@ -17,6 +25,7 @@ if __name__ == "__main__":
   import json
   import sys
   import os
+  import time
 
   import argparse
   parser = argparse.ArgumentParser(description='Create PVs from empty disks and add to (new or existing) VG.')
@@ -97,19 +106,26 @@ if __name__ == "__main__":
   #valid_disks bevat alle disks zonder partities (zowel in udev als in partitietabel) en die wel geopend konden worden.
   #disks bevat een dictionary van disks welek weer dictionarys zijn van prtities welke tuples zijn.
 
+  if len(valid_disks) == 0:
+    print('No suitable disk found. Please cleanup partitions before you proceed.'.format(d))
+
   if args.debug:
     print(json.dumps({'valid_disks':list(valid_disks), 'invalid_disks':list(invalid_disks), 'disks':disks}))
   if not args.info:
     parts=[]
     for d in valid_disks:
       try:
-        subprocess.check_call(['parted', '-s', '-m', '/dev/{0}'.format(d), '--','mklabel',args.pttable,'mkpart','lvm','1','-1'], stdout=devnull, stdin=devnull, stderr=devnull)
+        subprocess.check_call(['parted', '-s', '-m', '/dev/{0}'.format(d), '--','mklabel',args.pttype,'mkpart','lvm','1','-1', 'set', '1', 'lvm', 'on'], stdout=devnull, stdin=devnull, stderr=devnull)
+      except:
+        print('Failed to create new partition on {0}.'.format(d))
+        sys.exit(1)
+      try:
         part='/dev/{0}1'.format(d)
         parts.append(part)
-        subprocess.check_call(['pvcreate', part.format(d)], stdout=devnull, stdin=devnull, stderr=devnull)
+        subprocess.check_call(['pvcreate', '-y', '-ff', part], stdout=devnull, stdin=devnull, stderr=devnull)
       except:
-        print('Failed to create new partition on {0} or pv on new partition.'.format(d))
-        exit(1)
+        print('Failed to create pv on new partition.'.format(d))
+        sys.exit(1)
     
     if os.path.exists('/dev/{0}'.format(args.vg)):
       #device exists. Hope it's a vg and try to extend
@@ -117,14 +133,14 @@ if __name__ == "__main__":
         subprocess.check_call(['vgextend', '-y', args.vg]+parts, stdout=devnull, stdin=devnull, stderr=devnull)
       except:
         print('device /dev/{0} exists. Tried to extend, but couldnt. Maybe the vg name is invalid?'.format(args.vg))
-        os.exit(1)
+        sys.exit(1)
 
       #rediscover what PV's de VG is made of.
       parts=[]
       pvs=subprocess.Popen(['pvs', '-o', 'VG_Name,PV_Name', '--noheadings'], stdout=subprocess.PIPE, stdin=devnull, stderr=devnull)
       pvs_re=re.compile('{0} +([a-zA-Z0-9]+)'.format(args.vg))
       for line in pvs:
-        if line[-1] = '\n':
+        if line[-1] == '\n':
           line=line[:-1]
         m=pvs_re.search(line)
         if m:
@@ -134,16 +150,25 @@ if __name__ == "__main__":
         subprocess.check_call(['vgcreate', '-y', args.vg]+parts, stdout=devnull, stdin=devnull, stderr=devnull)
       except:
         print('Tried to create new vg {0}, but failed. Invalid VG name?'.format(args.vg))
-        os.exit(1)
+        sys.exit(1)
 
     if args.swap:
       try:
-        subprocess.check_call(['lvcreate', '-y', '-n', 'lv_swap', '-L', '500M', '-i', len(parts), args.vg]+parts, stdout=devnull, stdin=devnull, stderr=devnull)
-        subprocess.check_call(['mkswap', '/dev/{0}/lv_swap'.format(args.vg)]+parts, stdout=devnull, stdin=devnull, stderr=devnull)
-        subprocess.check_call(['swapon', '/dev/{0}/lv_swap'.format(args.vg)]+parts, stdout=devnull, stdin=devnull, stderr=devnull)
+        subprocess.check_call(['lvcreate', '-y', '-n', 'lv_swap', '-L', '500M', '-i', str(len(parts)), args.vg], stdout=devnull, stdin=devnull, stderr=devnull)
       except:
-        print('Tried to create, initiate and use swap /dev/{0}/lv_swap, but failed. Invalid VG name?'.format(args.vg))
-        os.exit(1)
+        print('Tried to create swap lv /dev/{0}/lv_swap, but failed. Invalid VG name?'.format(args.vg))
+        sys.exit(1)
+      time.sleep(1)
+      try:
+        subprocess.check_call(['mkswap', '/dev/{0}/lv_swap'.format(args.vg)], stdout=None, stdin=devnull, stderr=None)
+      except:
+        print('Tried to initiate swap lv /dev/{0}/lv_swap, but failed. Invalid VG name?'.format(args.vg))
+        sys.exit(1)
+      try:
+        subprocess.check_call(['swapon', '/dev/{0}/lv_swap'.format(args.vg)], stdout=devnull, stdin=devnull, stderr=devnull)
+      except:
+        print('Tried to use swap lv /dev/{0}/lv_swap, but failed. Invalid VG name?'.format(args.vg))
+        sys.exit(1)
 
     if args.root:
       i=1
@@ -155,49 +180,53 @@ if __name__ == "__main__":
           subprocess.check_call(['lvcreate', '-y', '-n', lv, '-l', '100%PVS', args.vg, part], stdout=devnull, stdin=devnull, stderr=devnull)
         except:
           print('Tried to create use rootfs lv /dev/{0}/{1}, but failed. Please investigate.'.format(args.vg, lv))
-          os.exit(1)
+          sys.exit(1)
 
         cryptsetup=subprocess.Popen(['cryptsetup', 'luksFormat', '/dev/{0}/{1}'.format(args.vg,lv), '-'], stdout=devnull, stdin=subprocess.PIPE, stderr=devnull)
-        cryptsetup.communicate('wachtwoord')
+        cryptsetup.communicate(bytes('wachtwoord', 'utf-8'))
         cryptsetup.communicate()
         cryptsetup.wait()
         if cryptsetup.returncode != 0:
           print('Tried to encrypt rootfs lv /dev/{0}/{1}, but failed. Please investigate.'.format(args.vg, lv))
-          os.exit(1)
+          sys.exit(1)
         cryptsetup=subprocess.Popen(['cryptsetup', 'open', '--type', 'luks', '--key-file', '-', '/dev/{0}/{1}'.format(args.vg,lv), cryptdev], stdout=devnull, stdin=subprocess.PIPE, stderr=devnull)
-        cryptsetup.communicate('wachtwoord')
+        cryptsetup.communicate(bytes('wachtwoord', 'utf-8'))
         cryptsetup.communicate()
         cryptsetup.wait()
         if cryptsetup.returncode != 0:
           print('Tried to open rootfs lv /dev/{0}/{1}, but failed. Please investigate.'.format(args.vg,lv))
-          os.exit(1)
+          sys.exit(1)
         cryptdevs.append('/dev/mapper/'+cryptdev)
       try:
         subprocess.check_call(['mkfs.btrfs'] + cryptdevs, stdout=devnull, stdin=devnull, stderr=devnull)
       except:
         print('Tried to create root btrfs filesystem, but failed. Please investigate..'.format(args.vg))
+        sys.exit(1)
       try:
         subprocess.check_call(['mount', cryptdevs[0], '/mnt/gentoo'], stdout=devnull, stdin=devnull, stderr=devnull)
       except:
         print('Tried to mount root btrfs filesystem, but failed. Please investigate..'.format(args.vg))
+        sys.exit(1)
       try:
         os.makedirs('/mnt/gentoo/__snapshots')
         subprocess.check_call(['btrfs', 'subvolume', 'create', '/mnt/gentoo/__active'], stdout=devnull, stdin=devnull, stderr=devnull)
         subprocess.check_call(['btrfs', 'subvolume', 'create', '/mnt/gentoo/__empty'], stdout=devnull, stdin=devnull, stderr=devnull)
         btrfs=subprocess.Popen(['btrfs', 'subvolume', 'list', '-t', '/mnt/gentoo'], stdin=devnull, stdout=subprocess.PIPE, stderr=devnull)
-        splitter_re=re.compile('[\t ]+')
+        splitter_re=re.compile('\t+')
         for line in btrfs.stdout:
+          line=line.decode('utf-8')
           if line[-1] == '\n':
             line=line[:-1]
-          ID, gen, top level, path = splitter_re.split(line)
+          ID, gen, top_level, path = splitter_re.split(line)[:4]
           if path=='__active':
-            subprocess.check_call(['btrfs', 'subvolume', 'set-default', ID, '/mnt/gentoo'], stdout=devnull, stdin=devnull, stderr=devnull)
+            subprocess.check_call(['btrfs', 'subvolume', 'set-default', ID, '/mnt/gentoo'], stdout=None, stdin=None, stderr=None)
             break
       except:
-        print('Tried to create root btrfs filesystem, but failed. Please investigate..'.format(args.vg))
+        print('Tried to init root btrfs filesystem, but failed. Please investigate..'.format(args.vg))
+        sys.exit(1)
       try:
         subprocess.check_call(['umount', '/mnt/gentoo'], stdout=devnull, stdin=devnull, stderr=devnull)
         subprocess.check_call(['mount', cryptdevs[0], '/mnt/gentoo'], stdout=devnull, stdin=devnull, stderr=devnull)
       except:
         print('Tried to remount root btrfs filesystem, but failed. Please investigate..'.format(args.vg))
-
+        sys.exit(1)
